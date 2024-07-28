321352491bd2a353501ea7d2d6464d7b9c54a0d7
Paulo Reis <pauloreisprofissional@gmail.com>
2024-07-27 21:31:02 -0300

    Criando nosso ORM

--- index.php
+++ index.php
@@ -1,118 +1,24 @@
 <?php
 
-require_once __DIR__ . '/database.php';
-require_once __DIR__ . '/funcoes/gerenciamento_produto.php';
+require 'vendor/autoload.php';
 
-try {
-    $pdo = new PDO($dsn, $user, $password, $options);
+use App\Models\CategoryModel;
+use App\Models\ProductModel;
 
-    $id = $_GET['id'] ?? '';
+$productModel = new ProductModel;
 
-    $idProdutoDeletar = $_GET['deletar'] ?? '';
+$novoProduto = [
+    'nome' => 'Celular S24+',
+    'descricao' => 'Smartphone com 512GB',
+    'preco' => 7999
+];
 
-    if (!empty($idProdutoDeletar)) {
-        deletarProduto($pdo, $idProdutoDeletar);
-    }
+$productModel->insert($novoProduto);
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $nomeDoProduto = $_POST['nome'];
-        $descricaoDoProduto = $_POST['descricao'];
-        $precoDoProduto = $_POST['preco'];
+var_dump($productModel->getAll());
+// var_dump($productModel->getById(3));
 
-        cadastrarProduto($pdo, $nomeDoProduto, $descricaoDoProduto, $precoDoProduto);
-    }
+// $categoryModel = new CategoryModel;
+// var_dump($categoryModel->getAll());
 
-    $produtos = obterTodosOsProdutos($pdo);
-
-} catch (PDOException $exception) {
-    die("Não foi possível se conectar com o banco de dados. Motivo: {$exception->getMessage()}");
-}
-
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha primeira aplicação com PHP + PDO + MySQL</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-
-    <div class="container">
-        
-        <div class="row">
-            <div class="col-md-12">
-
-                <h1 class="mt-5 mb-4">Meus produtos</h1>
-
-                <table class="table table-striped">
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome do produto</th>
-                        <th>Categoria do produto</th>
-                        <th>Preço do produto</th>
-                        <th>Ação</th>
-                    </tr>
-
-                    <?php foreach($produtos as $produto): ?>
-                    <tr>
-                        <td><?php echo $produto['id']; ?></td>
-                        <td><?php echo $produto['nome']; ?></td>
-                        <td><?php echo $produto['categoria']; ?></td>
-                        <td>R$ <?php echo number_format($produto['preco'], 2, ',', '.'); ?></td>
-                        <td>
-                            <a class="btn btn-primary btn-sm" href='atualizar.php?id=<?php echo $produto['id']; ?>'>Editar</a>
-                            <a onclick="confirmarDelecao('<?php echo $produto['id']; ?>')" class="btn btn-danger btn-sm" href='#'>Excluir</a>
-                        </td>
-                    </tr>
-                    <?php endforeach; ?>
-
-                </table>
-
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-12">
-
-                <h2 class="mt-5 mb-4">Cadastro de produto</h2>
-
-                <form action="" method="post">
-
-                    <div class="mb-3">
-                        <label for="nome" class="form-label">Produto</label>
-                        <input type="text" class="form-control" id="nome" name="nome">
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="descricao" class="form-label">Descrição</label>
-                        <input type="text" class="form-control" id="descricao" name="descricao">
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="preco" class="form-label">Preço</label>
-                        <input type="text" class="form-control" id="preco" name="preco">
-                    </div>
-
-                    <button class="btn btn-success">Cadastrar</button>
-
-                </form>
-
-            </div>
-        </div>
-
-    </div>
-
-    <script>
-        
-    function confirmarDelecao(idProduto) {
-        if (confirm('Você tem certeza que deseja excluir este produto?')) {
-            window.location = `index.php?deletar=${idProduto}`;
-        }
-    }
-
-    </script>
-    
-</body>
-</html>
+// var_dump($categoryModel->getById(2));
